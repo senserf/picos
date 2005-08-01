cd115c105195a130f5093ef45acfa43c8cce3387
dropbox <dropbox>
2005-08-01 05:55:38 +0000

    This file was not included in the previous commit, although it was needed

---
+++ PicOS/MSP430/p2irq.c
@@ -0,0 +1,18 @@
+#include "kernel.h"
+/*
+ * This code handles all interrupts triggered by P2 pins
+ */
+
+#if		DM2100
+#include	"dm2100.h"
+#endif
+
+interrupt (PORT2_VECTOR) p1_int () {
+
+#if	DM2100
+#include "irq_dm2100_pins.h"
+#endif
+
+// Here room for more functions
+
+}
