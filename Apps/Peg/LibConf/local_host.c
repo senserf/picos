6224b5a9492883c16297486322e022eaabe2ae00
dropbox <dropbox>
2006-10-04 20:16:13 +0000

    R061004A

--- Apps/Peg/LibConf/local_host.c
+++
@@ -1,7 +0,0 @@
-/* ==================================================================== */
-/* Copyright (C) Olsonet Communications, 2002 - 2004.                   */
-/* All rights reserved.                                                 */
-/* ==================================================================== */
-#include "sysio.h"
-#include "msg_tarp.h"
-nid_t	local_host = 1;
