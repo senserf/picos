3e9fda023beb255ecd4b0cc2b591d865bf8762b6
picos <picos>
2008-02-29 02:02:20 +0000

    R080228A

---
+++ PicOS/Libs/Stubs/msg_isNew.c
@@ -0,0 +1,15 @@
+/* ==================================================================== */
+/* Copyright (C) Olsonet Communications, 2002 - 2008.			*/
+/* All rights reserved.							*/
+/* ==================================================================== */
+
+// +++ "tarp.c"
+// For a stub, this is interpreted as a contingency, i.e., the stub is only
+// included in the library, if the indicated file belongs there as well
+
+#include "tarp.h"
+
+// Stubs for the "virtual" functions of TARP needed to shut up the Cyan
+// compiler
+
+Boolean msg_isNew (msg_t a) { return NO; }
---
+++ PicOS/Libs/Stubs/tr_offset.c
@@ -0,0 +1,16 @@
+/* ==================================================================== */
+/* Copyright (C) Olsonet Communications, 2002 - 2008.			*/
+/* All rights reserved.							*/
+/* ==================================================================== */
+
+// +++ "tarp.c"
+// For a stub, this is interpreted as a contingency, i.e., the stub is only
+// included in the library, if the indicated file belongs there as well
+
+#include "tarp.h"
+
+// Stubs for the "virtual" functions of TARP needed to shut up the Cyan
+// compiler
+
+int tr_offset (headerType *a) { return 0; }
+
