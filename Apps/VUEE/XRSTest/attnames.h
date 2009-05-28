539f1a3397ef83434f33398953cb29ffbcdd122e
picos <picos>
2009-05-28 17:44:09 +0000

    R090528A+

---
+++ Apps/VUEE/XRSTest/attnames.h
@@ -0,0 +1,28 @@
+#ifndef	__praxis_attnames_h__
+#define	__praxis_attnames_h__
+#ifdef	__SMURPH__
+
+#define SFD	_dac (Node, SFD)
+#define b	_dac (Node, b)
+#define chp	_dac (Node, chp)
+#define cc	_dac (Node, cc)
+#define ibuf	_dac (Node, ibuf)
+#define obuf	_dac (Node, obuf)
+#define adr	_dac (Node, adr)
+#define val	_dac (Node, val)
+#define u	_dac (Node, u)
+#define s	_dac (Node, s)
+#define pat	_dac (Node, pat)
+#define w	_dac (Node, w)
+#define err	_dac (Node, err)
+#define len	_dac (Node, len)
+#define bs	_dac (Node, bs)
+#define nt	_dac (Node, nt)
+#define sl	_dac (Node, sl)
+#define ss	_dac (Node, ss)
+#define dcnt	_dac (Node, dcnt)
+#define str	_dac (Node, str)
+#define blk	_dac (Node, blk)
+
+#endif
+#endif
