059fae426b3f742b134bd96186c87357f5c30682
picos <picos>
2009-06-06 20:43:47 +0000

    *** empty log message ***

--- Apps/VUEE/eco_demo/globals_peg.h
+++ Apps/VUEE/eco_demo/globals_peg.h
@@ -76,21 +76,21 @@ static const char welcome_str[] = OPRE_APP_MENU_A
 	OPRE_APP_MENU_A 
 	"\tDisplay data:\tD [ from [ to [ col_id [ limit ]]]]\r\n"
 	OPRE_APP_MENU_A 
-	"\tMaintenance:\tM (* No collection until F     *)\r\n"
+	"\tMaintenance:\tM (* No collection until F *)\r\n"
 	OPRE_APP_MENU_A
-	"\tEprom erase:\tE (* deletes aggregated data   *)\r\n"
+	"\tEprom erase:\tE (* deletes aggregated data *)\r\n"
 	OPRE_APP_MENU_A 
 	"\tFlash erase:\tF (* clears special conditions *)\r\n"
 	OPRE_APP_MENU_A 
 	"\tClean reset:\tQ (* to factory defaults (E+F) *)\r\n"
 	OPRE_APP_MENU_A
-	"\tID set / show:\tI[D id]   (* CAREFUL Host ID   *)\r\n"
+	"\tID set / show:\tI[D id]   (* CAREFUL Host ID *)\r\n"
 	OPRE_APP_MENU_A
 	"\tID master set:\tIM id     (* CAREFUL Master ID *)\r\n"
 	OPRE_APP_MENU_A 
 	"\tSave(d) sys:  \tS[A]      (* Show, SAve iFLASH *)\r\n"
 	OPRE_APP_MENU_A
-	"\tSync coll:    \tY [freq]  (* Sync at freq      *)\r\n"
+	"\tSync coll:    \tY [freq]  (* Sync at freq *)\r\n"
 	OPRE_APP_MENU_A 
 	"\tQuit (reset)\tq\r\n"
 	OPRE_APP_MENU_A 
