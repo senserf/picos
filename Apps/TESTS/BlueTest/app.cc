7060e672f9c3b44e8faeeb3c0574d7791dc1bede
Pawel Gburzynski <pawel@olsonet.com>
2010-07-14 13:35:54 -0700

    PG100714A: board WARSAW_NEW_BLUE + Apps/TEST/BlueTest (testing Bluetooth connections with the Android); + some rather irrelavant cosmetics.

---
+++ Apps/TESTS/BlueTest/app.cc
@@ -0,0 +1,132 @@
+/* ==================================================================== */
+/* Copyright (C) Olsonet Communications, 2002 - 2010                    */
+/* All rights reserved.                                                 */
+/* ==================================================================== */
+
+#include "sysio.h"
+#include "pinopts.h"
+
+#include "ser.h"
+#include "serf.h"
+#include "form.h"
+
+#define	IBUFLEN	82
+
+trueconst word	RATE115	= 1152,
+		RATE096 = 96;
+
+fsm minput {
+
+  shared char c;
+
+  entry MI_READ:
+
+	io (MI_READ, UART_B, READ, &c, 1);
+
+  entry MI_WRITE:
+
+	io (MI_WRITE, UART_A, WRITE, &c, 1);
+
+	proceed MI_READ;
+}
+
+fsm root {
+
+	shared char *ibuf;
+	shared int n, k, p;
+
+  entry RS_INI:
+
+	ibuf = (char*) umalloc (IBUFLEN + 2);
+	//ion (UART_A, CONTROL, (char*)(&RATE096), UART_CNTRL_SETRATE);
+	//ion (UART_B, CONTROL, (char*)(&RATE096), UART_CNTRL_SETRATE);
+	runfsm minput;
+
+  entry RS_MEN:
+
+	ser_out (RS_MEN,
+		"\r\nBlueTooth Test\r\n"
+		"Commands:\r\n"
+		"w string    -> write line to module\r\n"
+		"r rate      -> set rate for module\r\n"
+		"t rate      -> set rate for UART\r\n"
+		"e [0|1]     -> escape\r\n"
+		"s           -> reset module\r\n"
+		"a           -> view attention flag\r\n"
+	);
+
+  entry RS_RCM:
+
+	ser_in (RS_RCM, ibuf, IBUFLEN-1);
+
+	switch (ibuf [0]) {
+
+	    case 'w': proceed RS_WRI;
+	    case 'r': proceed RS_RAT;
+	    case 't': proceed RS_RAU;
+	    case 'e': proceed RS_ESC;
+	    case 's': proceed RS_RES;
+	    case 'a': proceed RS_ATT;
+
+	}
+
+  entry RS_ERR:
+
+	ser_out (RS_ERR, "Illegal command or parameter\r\n");
+	proceed RS_MEN;
+
+  entry RS_WRI:
+
+	for (k = 1; ibuf [k] == ' '; k++);
+	n = strlen (ibuf + k);
+
+	ibuf [k + n    ] = '\r';
+	ibuf [k + n + 1] = '\n';
+
+	n += 2;
+	ibuf [k + n    ] = '\0';
+
+  entry RS_WRL:
+
+	while (n) {
+		p = io (RS_WRL, UART_B, WRITE, ibuf + k, n);
+		n -= p;
+		k += p;
+	}
+
+	proceed RS_RCM;
+
+  entry RS_RAT:
+
+	n = 96;
+	scan (ibuf + 1, "%d", &n);
+	ion (UART_B, CONTROL, (char*)(&n), UART_CNTRL_SETRATE);
+	proceed RS_RCM;
+
+  entry RS_RAU:
+
+	n = 96;
+	scan (ibuf + 1, "%d", &n);
+	ion (UART_A, CONTROL, (char*)(&n), UART_CNTRL_SETRATE);
+	proceed RS_RCM;
+
+  entry RS_ESC:
+
+	n = 0;
+	scan (ibuf + 1, "%d", &n);
+	if (n)
+		blue_cmdmode;
+	else
+		blue_datamode;
+	proceed RS_RCM;
+
+  entry RS_RES:
+
+	blue_reset;
+	proceed RS_RCM;
+
+  entry RS_ATT:
+
+	ser_outf (RS_ATT, "ATT: %d", blue_attention != 0);
+	proceed RS_RCM;
+}
---
+++ PicOS/MSP430/BOARDS/WARSAW_NEW_BLUE/board_pins.h
@@ -0,0 +1,128 @@
+/* ==================================================================== */
+/* Copyright (C) Olsonet Communications, 2002 - 2010                    */
+/* All rights reserved.                                                 */
+/* ==================================================================== */
+
+// Includes BlueTooth LinkMatic 2.0 connected via J3:
+//
+//	GND  ->       (0)
+//	P3.7 -> TX    (1)
+//	P3.6 -> RX    (2)
+// 	P5.6 -> Reset (3)
+//	P2.5 -> ESC   (4)
+//	P2.6 -> ATTN  (5)
+//	Vcc  ->       (9)
+//
+
+// ============================================================================
+// Pin allocation (Sep 30, 2009):
+//
+//	J3: 	============= LCD side
+//		VUSB	RXUA
+//		TXUA  	GND
+//		Vcc	Veref+
+//		P3.7	P3.6
+//		P5.6	P2.5
+//		P2.6	P4.0
+//		P4.4	P4.5
+//		P4.6	Vref-
+//		Vcc  	GND
+//		=============
+//
+//	J5:	============= External side
+//              GND     P1.6
+//		P4.7	P6.7
+//              GND	P6.2
+//		P6.6	P6.0
+//	        GND	P6.4
+//	        GND	P6.1
+//	        GND	P6.3
+//	        GND	P6.5
+//		=============
+
+
+// ============================================================================
+// CC1100 on P1
+// ============================================================================
+// 0 = RF SI OUT
+// 1 = RF CLK OUT
+// 2 = RF SO IN
+// 3 = RF GDO2 unused, but must be input
+// 4 = RF GDO0 IN
+// 5 = RF CSN OUT
+// 6,7 NC
+#define	PIN_DEFAULT_P1DIR	(0x23+0xC0)
+
+// P2.0 INT1 RTC
+// P2.1 SCL RTC
+// P2.2 INT2 RTC
+// P2.3 OPTIONS BUTTON
+// P2.4 SOFT RESET
+// P2.5 Blue ESC
+// P2.6 Blue ATTN
+// P2.7 SDA RTC
+#define	PIN_DEFAULT_P2DIR	0x20	// ESC is OUT
+
+// Soft reset
+#define	SOFT_RESET_BUTTON_PRESSED	((P2IN & 0x10) == 0)
+
+// P3.0 POWER Analog SWITCH 4	LCD
+// P3.1 POWER Analog SWITCH 2	EE
+// P3.2 POWER Analog SWITCH 1	SD
+// P3.3 POWER Analog SWITCH 3 	GPS
+// P3.4 U0 TX
+// P3.5 U0 RX
+// P3.6 U1 TX (Blue)
+// P3.7 U1 RX (Blue)
+#define PIN_DEFAULT_P3DIR	0x5f
+
+// P4.0 LCD  and CONN
+// P4.1 LED
+// P4.2 LED
+// P4.3 LED
+// P4.4 LCD  and CONN
+// P4.5 LCD  and CONN
+// P4.6 LCD  and CONN
+// P4.7 NC
+#define	PIN_DEFAULT_P4DIR	0x8e
+
+// P5.0 EPR CS
+// P5.1 EPR/SD SI
+// P5.2 EPR/SD SO
+// P5.3 EPR/SD CLK
+// P5.4 SEN VRGEN
+// P5.5 SD CS
+// P5.6 Blue Reset
+// P5.7 Voltage sensor ON (LOW!!)
+#define	PIN_DEFAULT_P5DIR	0x40
+
+// NC
+#define	PIN_DEFAULT_P6DIR	0xFF
+
+// Bluetooth special pins
+#define	blue_cmdmode		_BIS (P2OUT,0x20)
+#define	blue_datamode		_BIC (P2OUT,0x20)
+#define	blue_reset		do { \
+					_BIS(P5OUT, 0x40); \
+					mdelay (30); \
+					_BIC(P5OUT, 0x40); \
+				} while (0)
+#define	blue_attention		(P2IN & 0x40)
+
+
+#define	PIN_MAX			0
+#define	PIN_MAX_ANALOG		0
+#define	PIN_DAC_PINS		0
+
+//
+// Component power switch (on P3)
+//
+#define	CSWITCH_LCD		1	// P3.0
+#define CSWITCH_EE		2	// P3.1
+#define	CSWITCH_SD		4	// P3.2
+#define	CSWITCH_GPS		8	// P3.3
+
+#define	cswitch_on(p)		_BIS (P3OUT, (p) & 0x0f)
+#define	cswitch_off(p)		_BIC (P3OUT, (p) & 0x0f)
+
+#define	CSWITCH_ALL	(CSWITCH_LCD + CSWITCH_EE + CSWITCH_SD + CSWITCH_GPS)
