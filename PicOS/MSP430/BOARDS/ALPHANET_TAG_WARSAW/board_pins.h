a6ba3b99f438ddb175187423f122835267297cde
Pawel Gburzynski <pawel@olsonet.com>
2015-10-22 14:27:56 +0200

    PG151022B:
    	Fixed shared_plug in locaspot to properly recognize location readings
    	piggybacked onto event packets (as well whatever it recognized before).
    
    	Fixed the bug in oss_peg_tcve.cc resulting in the incorrect values in
    	location readings piggubacked onto events.
    
    	Added a new board ALPHANET_TAG_WARSAW (associated with BUT1 in
    	locaspot) to let us use the WARSAW box as a Tag.

---
+++ PicOS/MSP430/BOARDS/ALPHANET_TAG_WARSAW/board_pins.h
@@ -0,0 +1,139 @@
+/* ==================================================================== */
+/* Copyright (C) Olsonet Communications, 2002 - 2009                    */
+/* All rights reserved.                                                 */
+/* ==================================================================== */
+
+// Departures from default pre-initialization
+
+#if CC1100
+
+// ============================================================================
+// Radio is on P1
+// ============================================================================
+// 0 = RF SI OUT
+// 1 = RF CLK OUT
+// 2 = RF SO IN
+// 3 = RF GDO2 unused, but must be input
+// 4 = RF GDO0 IN
+// 5 = RF CSN OUT
+// 6,7 General, unused by default
+#define	PIN_DEFAULT_P1DIR	0xE3
+
+#else
+
+#define	PIN_DEFAULT_P1DIR	0xFF
+
+#endif
+
+// ============================================================================
+
+// 0, 1, 7 hang loose, 4 = soft reset button, must be IN
+// 2, 3, 5, 6 general, unused by default
+#define	PIN_DEFAULT_P2DIR	0xEF
+
+#define	SOFT_RESET_BUTTON_PRESSED	((P2IN & 0x10) == 0)
+
+// ============================================================================
+
+// 0 doubles with RXD1 (as CTS, and is in the way, so must be input)
+// 1 doubles with TXD0 (and is in the way, so must be input)
+// 2 doubles with RXD0 (and is in the way, so must be input)
+// 3 doubles with TXD1 ....
+// 4 is TXD0 (output)
+// 5 is RXD0 (input)
+// 6 is TXD1
+// 7 is RXD1
+#define	PIN_DEFAULT_P3DIR	0x50
+
+// 1, 2, 3 = LEDs, 0, 4-7 = general unused by default
+#define PIN_DEFAULT_P4DIR	0xFF
+#define	PIN_DEFAULT_P4OUT	0x0E	// LEDs off (high) by default
+
+// ============================================================================
+// P5 used by EEPROM/SDCARD
+// ============================================================================
+// 0 = EEPROM CS OUT
+// 1 = STORAGE SI OUT
+// 2 = STORAGE SO IN
+// 3 = STORAGE SCK OUT
+// 4 = SD card CS OUT
+#define	PIN_DEFAULT_P5DIR	0xFB
+//#define	PIN_DEFAULT_P5OUT	0x01	// Default CS is up
+#define	PIN_DEFAULT_P5OUT	0x0B	// Default both CS, CLK are up
+
+#define	BUTTON_LIST 	{ BUTTON_DEF (1, 0x40, 0) }
+#define	BUTTON_PIN_P1_IRQ	0x40
+#define	BUTTON_DEBOUNCE_DELAY	32
+#define	BUTTON_PRESSED_LOW	0
+#define	BUTTON_PANIC		0
+
+#if ADC_SAMPLER
+
+#define	PIN_DEFAULT_P6DIR	0x00
+
+#define	PIN_LIST	{	\
+	PIN_DEF	(P1, 7),	\
+	PIN_DEF	(P2, 2),	\
+	PIN_DEF	(P2, 3),	\
+	PIN_DEF	(P2, 5),	\
+	PIN_DEF	(P2, 6),	\
+	PIN_DEF	(P4, 0),	\
+	PIN_DEF	(P4, 4),	\
+	PIN_DEF	(P4, 5),	\
+	PIN_DEF	(P4, 6),	\
+	PIN_DEF	(P4, 7),	\
+	PIN_DEF	(P5, 4),	\
+}
+
+#define	PIN_MAX			11	// Number of pins
+#define	PIN_MAX_ANALOG		0	// Number of available analog pins
+#define	PIN_DAC_PINS		0
+
+#else	/* NO SAMPLER */
+
+#define	PIN_DEFAULT_P6DIR	0xFF
+
+#define	PIN_LIST	{	\
+	PIN_DEF	(P6, 0),	\
+	PIN_DEF	(P6, 1),	\
+	PIN_DEF	(P6, 2),	\
+	PIN_DEF	(P6, 3),	\
+	PIN_DEF	(P6, 4),	\
+	PIN_DEF	(P6, 5),	\
+	PIN_DEF	(P6, 6),	\
+	PIN_DEF	(P6, 7),	\
+	PIN_DEF	(P1, 7),	\
+	PIN_DEF	(P2, 2),	\
+	PIN_DEF	(P2, 3),	\
+	PIN_DEF	(P2, 5),	\
+	PIN_DEF	(P2, 6),	\
+	PIN_DEF	(P4, 0),	\
+	PIN_DEF	(P4, 4),	\
+	PIN_DEF	(P4, 5),	\
+	PIN_DEF	(P4, 6),	\
+	PIN_DEF	(P4, 7),	\
+	PIN_DEF (P5, 4)		\
+}
+
+#define	PIN_MAX			19	// Number of pins
+#define	PIN_MAX_ANALOG		8	// Number of available analog pins
+#define	PIN_DAC_PINS		0x0706	// Two DAC pins: #6 and #7
+
+#endif /* SAMPLER or no SAMPLER */
+
+// ============================================================================
+#if 1
+
+#include "analog_sensor.h"
+#include "sensors.h"
+
+#define	SENSOR_LIST { \
+		INTERNAL_TEMPERATURE_SENSOR,	\
+		INTERNAL_VOLTAGE_SENSOR		\
+	}
+#define	SENSOR_ANALOG
+#define	N_HIDDEN_SENSORS	2
+
+#endif
+// ============================================================================
+
---
+++ PicOS/MSP430/BOARDS/ALPHANET_TAG_WARSAW/board_rf.h
@@ -0,0 +1,42 @@
+/*
+ * RF pins assignment:
+ *
+ *        RSI	P1.0	OUT
+ *	  RSCLK	P1.1	OUT
+ *	  RSO	P1.2	IN
+ *	  GDO2	P1.3	UNUSED
+ *	  GDO0	P1.4	IN
+ *	  CSN	P1.5	OUT
+ */
+
+//    Make sure P1 interrupts are serviced
+//+++ "p1irq.c"
+
+REQUEST_EXTERNAL (p1irq);	// Needed to force the file to be loaded
+				// if loading from library
+
+#define	cc1100_ini_regs		_BIC (P1IES, 0x10)
+
+#define	cc1100_int		(P1IFG & 0x10)
+#define	cc1100_clear_int	_BIC (P1IFG, 0x10)
+#define	CC1100_RX_FIFO_READY	(P1IN & 0x10)
+
+#define cc1100_rcv_int_enable	do { \
+					_BIS (P1IE, 0x10); \
+					if (CC1100_RX_FIFO_READY) \
+						_BIS (P1IFG, 0x10); \
+				} while (0)
+						
+#define cc1100_rcv_int_disable		_BIC (P1IE, 0x10)
+
+#define	cc1100_sclk_up		_BIS (P1OUT, 0x02)
+#define	cc1100_sclk_down	_BIC (P1OUT, 0x02)
+
+#define	cc1100_csn_up		_BIS (P1OUT, 0x20)
+#define	cc1100_csn_down	_BIC (P1OUT, 0x20)
+
+#define	cc1100_si_up		_BIS (P1OUT, 0x01)
+#define	cc1100_si_down		_BIC (P1OUT, 0x01)
+
+#define	cc1100_so_val		(P1IN & 0x04)
+
