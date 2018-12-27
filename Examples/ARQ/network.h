5be235ab22d9e39d39169e4c7e9b8e236c4a7000
Pawel Gburzynski <pawel@olsonet.com>
2018-12-27 12:09:23 +0100

    PG181227A:
    SMURPH has gone GNU PL V3. This is in preparation for the print of
    my new book on SMURPH (by Springer).
    
    - I have added the standard GNU PL V3 copyright notice to all
      relevant files. Needless to say, this has been done by a script.
    
    - Updated the book files. Note: those files will be removed when
      SMURPH is turned into a version to be put on Springer servers.
      In other words, the present version is not official.
    
    No other changes.

--- Examples/ARQ/network.h
+++ Examples/ARQ/network.h
@@ -1,3 +1,22 @@
+/*
+	Copyright 1995-2018, 2019 Pawel Gburzynski
+
+	This file is part of SMURPH/SIDE.
+
+	SMURPH/SIDE is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	SMURPH/SIDE is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with SMURPH/SIDE. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Network types
 
 packet	Frame {
--- SOURCES/version.h
+++ SOURCES/version.h
@@ -1,5 +1,24 @@
+/*
+	Copyright 1995-2018, 2019 Pawel Gburzynski
+
+	This file is part of SMURPH/SIDE.
+
+	SMURPH/SIDE is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	SMURPH/SIDE is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with SMURPH/SIDE. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* SIDE version number */
-#define  VERSION      "3.9-AB"
+#define  VERSION      "3.9-AG"
 
 #ifdef	__CYGWIN32__
 #define	ZZ_CYW				1
@@ -142,4 +161,4 @@ typedef void (*SIGARG) (int);
 #define  ZZ_XINCPAT      {"/home/nripg/SOFTWARE/PICOS/Apps/DataLib", NULL}
 #define  ZZ_MONHOST      "localhost"
 #define  ZZ_MONSOCK      4442
-#define  ZZ_RTAG	"PG180611A"
+#define  ZZ_RTAG	"PG181104A"
